c4f800a711e82a4acf109ccbe5ab18e113d1c420
paragulg <parag06rohim@gmail.com>
2023-03-06 18:55:27 +0300

    Second task

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+const Header = () => {
+  return (
+    <Button variant="contained">
+      <h1>Hello, World!</h1>
+    </Button>
+  );
+};
+
+export default Header;
