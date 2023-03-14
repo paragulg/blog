108fe8e35727e23c9d4c13793d9f760feb25be49
paragulg <parag06rohim@gmail.com>
2023-03-14 21:53:58 +0300

    Adding posts

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,11 @@ import Posts from './components/Posts';
 import './App.css';
 
 function App() {
-  const [inputName, setInputName] = React.useState('');
-  const [inputText, setInputText] = React.useState('');
-
-  const [cards, setCards] = React.useState([{ name: inputName, text: inputText }]);
-
-  const publish = () => {
-    setCards([...cards, { name: inputName, text: inputText }]);
-  };
 
   return (
     <div className="App">
       <Header />
-      <Posts
-        inputName={inputName}
-        setInputName={setInputName}
-        inputText={inputText}
-        setInputText={setInputText}
-        publish={publish}
-        cards={cards}
-      />
+      <Posts />
     </div>
   );
 }
--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -10,7 +10,20 @@ import {
   Typography,
 } from '@mui/material';
 
-const Posts = ({ inputName, setInputName, inputText, setInputText, publish, cards }) => {
+const Posts = () => {
+  const [inputName, setInputName] = React.useState('');
+  const [inputText, setInputText] = React.useState('');
+
+  const [cards, setCards] = React.useState([{ name: '', text: '' }]);
+
+  const publish = () => {
+    if (inputName !== '' && inputText !== '') {
+      if (cards[0].name == '' && cards[0].text == '') {
+        setCards([{ name: inputName, text: inputText }]);
+      } else setCards([...cards, { name: inputName, text: inputText }]);
+    }
+  };
+
   return (
     <div className="posts">
       <Box sx={{ width: 500, height: 400, backgroundColor: 'white', mt: -40 }}>
@@ -38,30 +51,34 @@ const Posts = ({ inputName, setInputName, inputText, setInputText, publish, card
           />
         </center>
         <center>
-          <Button sx={{ m: 2.5 }} onClick={publish} variant="contained">
+          <Button sx={{ m: 2.5 }} onClick={() => publish()} variant="contained">
             Опубликовать
           </Button>
         </center>
       </Box>
       <div className="cards">
-        {cards.map((entry, index) => (
-          <Card key={index} sx={{ width: 500, height: 400, mt: 5, mb: 5 }}>
-            <CardContent>
-              <CardActions sx={{ width: 'max-content' }}>
-                <Button className="deleteButton" size="small" variant="outlined">
-                  Удалить
-                </Button>
-                <Button className="editButton" size="small" variant="outlined">
-                  Редактировать
-                </Button>
-              </CardActions>
-              <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
-                {entry.name}
-              </Typography>
-              <Typography variant="h6">{entry.text}</Typography>
-            </CardContent>
-          </Card>
-        ))}
+        {cards.length > 0 && cards[0].name !== '' && cards[0].text !== '' ? (
+          cards.map((entry, index) => (
+            <Card key={index} sx={{ width: 500, height: 400, mt: 5, mb: 5 }}>
+              <CardContent>
+                <CardActions sx={{ width: 'max-content' }}>
+                  <Button className="deleteButton" size="small" variant="outlined">
+                    Удалить
+                  </Button>
+                  <Button className="editButton" size="small" variant="outlined">
+                    Редактировать
+                  </Button>
+                </CardActions>
+                <Typography sx={{ fontSize: 24 }} color="text.secondary" gutterBottom>
+                  {entry.name}
+                </Typography>
+                <Typography variant="h6">{entry.text}</Typography>
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <div className="empty">Нет постов</div>
+        )}
       </div>
     </div>
   );
